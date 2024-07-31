fae85ae90ea78b82aa27149238183ec81c8c4670
Miguel Tulla <miguel12a.tulla@gmail.com>
2024-07-31 10:51:58 -0400

    Added pipenv folder to venv-selector.

--- lua/mtulla/plugins/venv-selector.lua
+++ lua/mtulla/plugins/venv-selector.lua
@@ -1,11 +1,12 @@
 return {
 	"linux-cultist/venv-selector.nvim",
 	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {
-		-- Your options go here
-		-- name = "venv",
-		-- auto_refresh = false
-	},
+	config = function()
+		require("venv-selector").setup({
+			pipenv_path = "/Users/Miguel/.local/share/virtualenvs/",
+			pyenv_path = "/Users/Miguel/.pyenv/versions/",
+		})
+	end,
 	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 	keys = {
 		-- Keymap to open VenvSelector to pick a venv.
